8e23a2664ff60d4c91ea1718bb7b6c9653300cf0
Sewar77 <sewarjihad793@gmail.com>
2025-05-14 17:49:22 +0300

    commit blog with API

--- Blog with API/app.js
+++ Blog with API/app.js
@@ -1,63 +1,34 @@
 import express from "express";
-import bodyParser from "body-parser";
-import axios from "axios";
+import methodOverride from "method-override";
+
+import postRouter from "./routes/posts.js";
 
 const app = express();
-app.use(bodyParser.json());
 const port = 3000;
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
-let posts = [];
+app.use("/posts", postRouter);
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
+app.get("/newPost", (req, res) => {
+  res.render("newPost.ejs");
+});
+
 app.get("/viewPost", (req, res) => {
-  res.render("viewPost.ejs", { posts });
+  res.render("viewPosts.ejs");
 });
 
-app.get("/createPost", (req, res) => {
-  res.render("createPost.ejs");
+
+
+app.get("/editPost", (req, res) => {
+  res.render("editPost.ejs");
 });
 
-app.post("/createPost/:id", (req, res) => {
-  const { title, content, nameAuthr } = req.body;
-  if (!title || !content || !nameAuthr) {
-    return res.status(400).json({ error: "All fields are required" });
-  }
-  const post = {
-    id: posts.length + 1,
-    title,
-    content,
-    nameAuthr,
-  };
-  posts.push(post);
-  res.redirect("/createPost");
-});
-app.put("/updatePost/:id", (req, res) => {
-  const index = parseInt(req.params.id);
-  const postIndex = posts.findIndex((post) => post.id === index);
-  if (postIndex === -1) {
-    return res.status(404).json({ error: "Post not found" });
-  }
-  const { title, content, nameAuthr } = req.body;
-  if (!title || !content || !nameAuthr) {
-    return res.status(400).json({ error: "All fields are required" });
-  }
-  posts[postIndex] = { id: postIndex, title, content, nameAuthr };
-  res.redirect("/viewPost");
-});
 
-app.delete("/deletePost/:id", (req, res) => {
-  const index = parseInt(req.params.id);
-  if (index < 0 || index >= posts.length) {
-    return res.status(404).json({ error: "Post not found" });
-  }
-  posts.splice(index, 1);
-  res.redirect("/viewPost");
-});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
